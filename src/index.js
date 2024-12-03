1f06bc3ad4e35a0c7de516306a955b2251994e92
Aathira <23041002@myrp.edu.sg>
2024-12-03 22:29:49 +0800

    hi

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css'; // Import global styles
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Ensure the root element exists in index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
